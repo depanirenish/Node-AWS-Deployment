31590326daad8ae8e5d673e8c83b9c823b28758c
renishdepani <depanirenish@gmail.com>
2023-12-13 18:48:31 +0530

    chn-4

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello Elox");
+  res.send("Hello Elox_1");
 });
 app.get("/new", function (req, res) {
   res.send("New also worked");
